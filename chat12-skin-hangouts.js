98258bb390c8a7961ae013a1e639f1070937e6a0
Pascoual <pascoualito@gmail.com>
2015-01-26 12:11:31 +0100

    Add: contacts list Template

--- chat12-skin-hangouts.js
+++ chat12-skin-hangouts.js
@@ -1,15 +1,71 @@
+/**
+ * Global templates helpers
+ */
+Template.registerHelper("chat12GetContactName", function (user) {
+  return Chat12.getContactName(user);
+});
+Template.registerHelper("chat12GetContactAvatar", function (user) {
+  return Chat12.getContactAvatar(user);
+});
+Template.registerHelper("chat12GetUnreadNumber", function () {
+  return 0;
+});
+
+/**
+ * chatContactList
+ */
+Template.chatContactList.helpers({
+  getContacts: function (event, tmpl) {
+    var sort = {};
+    sort[Chat12.getContactOrderField()] = 1;
+    return Meteor.users.find({_id: {$in: Chat12.getContacts(this.userId)}}, {sort: sort});
+  },
+  getListClass: function (event, tmpl) { return Chat12.getContactsListClass()}
+});
+
+/**
+ * chatContactList
+ */
+Template.chat12Contact.events({
+  'click li': function (event, tmpl) {
+    Blaze.renderWithData(Template.chatContainer, tmpl.data, document.getElementById("chat12Zone"));
+  }
+});
+
+/**
+ * chatZoneBottom
+ */
+Template.chatZoneBottom.created = function () {
+  // subscribe
+  this.subscriptions = [Meteor.subscribe("chat12GetOnes")];
+};
+
+Template.chatZoneBottom.destroyed = function () {
+  // unsubscribe
+  this.subscriptions.forEach(function (subscription) {subscription.stop()});
+};
+
+/**
+ * chatContainer
+ */
 Template.chatContainer.rendered = function () {
   this.$(".chat-box").scrollTop(this.$(".chat-box").height() + 100);
 }
 
 Template.chatContainer.events({
   'click .buttonMinimize': function (event, tmpl) {
+    /*if (parseInt(tmpl.$(".chat-container .top-header").css('margin-top')) > 0)
+      tmpl.$(".chat-container .top-header").animate({"margin-top": 0}, 300);
+    else {
+      var height = tmpl.$(".chat-container").height() - tmpl.$(".top-header").height();
+      tmpl.$(".chat-container .top-header").animate({"margin-top": height}, 300);
+    }*/
     tmpl.$(".setting").toggle(300);
     tmpl.$(".chat-box").toggle(300);
     tmpl.$(".messagebox").toggle(300);
   },
   'click .buttonClose': function (event,tmpl) {
-    tmpl.$("chat-container").remove();
+    tmpl.$(".chat-container").remove();
   },
   /*
    * Try to get chat-container resizable via mouse :
--- package.js
+++ package.js
@@ -7,9 +7,10 @@ Package.describe({
 Package.on_use(function (api) {
   api.versionsFrom('METEOR@1.0');
 
-  api.use(['livedata', 'deps', 'templating', 'ui', 'blaze', 'ejson', 'reactive-var','jquery','raix:handlebar-helpers','fortawesome:fontawesome'], 'client');
+  api.use(['livedata', 'deps', 'templating', 'ui', 'blaze', 'ejson', 'reactive-var','jquery','raix:handlebar-helpers','fortawesome:fontawesome','mizzao:user-status','pascoual:chat12'], 'client');
 
   api.imply('pascoual:chat12');
-  
+  api.imply('mizzao:user-status')
+
   api.add_files(['chat12-skin-hangouts.css', 'chat12-skin-hangouts.html', 'chat12-skin-hangouts.js'], 'client');
 });
